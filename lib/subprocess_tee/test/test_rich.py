b721ad295e61ba6ea8a055613fce504623d37eb1
Sorin Sbarnea <ssbarnea@redhat.com>
2020-10-31 19:26:39 +0000

    Add testing of rich html_export

--- lib/subprocess_tee/test/test_rich.py
+++ lib/subprocess_tee/test/test_rich.py
@@ -16,3 +16,21 @@ def test_rich_console_ex() -> None:
     assert proc.stdout == "123\n"
     text = console.export_text()
     assert text == "alpha\nbeta\ngamma\ndelta\n123\n"
+
+
+def test_rich_console_ex_ansi() -> None:
+    """Validate that ANSI sent to sys.stdout does not become garbage in record."""
+    print()
+    console = ConsoleEx(force_terminal=True, record=True, redirect=True)
+    console.print("[green]this from Console.print()[/green]", style="red")
+    proc = run(r'echo -e "\033[31mred\033[0m"')
+    assert proc.returncode == 0
+    assert proc.stdout == "\x1b[31mred\x1b[0m\n"
+
+    # validate that what rich recorded is the same as what the subprocess produced
+    text = console.export_text(clear=False)
+    assert "\x1b[31mred\x1b[0m\n" in text
+
+    # validate that html export also contains at least the "red" text
+    html = console.export_html(clear=False)
+    assert "red" in html
