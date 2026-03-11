2359e1ab3ccceda86a2c51a13c050610abbff722
Eric Wieser <efw@google.com>
2026-03-11 01:35:42 +0000

    fix: throw a TypeError if unexpected arguments are passed

--- src/subprocess_tee/_types.py
+++ src/subprocess_tee/_types.py
@@ -2,24 +2,9 @@
 
 # Source from https://github.com/python/typing/issues/256#issuecomment-1442633430
 from collections.abc import Iterator, Sequence
-from typing import Any, Protocol, SupportsIndex, TypeVar, overload
+from typing import Any, Protocol, SupportsIndex, TypeVar, overload, Union
+from os import PathLike
 
 _T_co = TypeVar("_T_co", covariant=True)
 
-
-class SequenceNotStr(Protocol[_T_co]):
-    """Lists of strings which are not strings themselves."""
-
-    @overload
-    def __getitem__(self, index: SupportsIndex, /) -> _T_co: ...
-    @overload
-    def __getitem__(self, index: slice, /) -> Sequence[_T_co]: ...
-    def __contains__(self, value: object, /) -> bool: ...
-    def __len__(self) -> int: ...
-    def __iter__(self) -> Iterator[_T_co]: ...
-    def index(  # pylint: disable=C0116
-        self, value: Any, start: int = 0, stop: int = ..., /
-    ) -> int: ...
-    def count(self, value: Any, /) -> int: ...  # pylint: disable=C0116
-
-    def __reversed__(self) -> Iterator[_T_co]: ...
+StrOrBytesPath = Union[str, bytes, PathLike[str], PathLike[bytes]]
